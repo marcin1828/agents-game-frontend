71a2e37444cca95aa8542d7f0b4c764fcaec2cbb
Marcin Miernik <marcin.miernik2@gmail.com>
2021-12-23 03:08:11 +0100

    Add Lobby view

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import GameService from './views/GameService/GameService';
 import StartView from './views/StartView/StartView';
+import Lobby from './views/Lobby/Lobby';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartView />}/>
+        <Route path="lobby" element={<Lobby />}/>
         <Route path="game" element={<GameService />}>
           <Route path=":id" element={<GameService />}/>
         </Route>
---
+++ src/views/Lobby/Lobby.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Lobby.module.scss';
+import logo from '../../assets/images/logo.png';
+
+
+const Lobby = () => {
+    return (
+        <div className={styles.wrapper}>
+            <img className={styles.logo} src={logo} alt='Logo Agents'/>
+            <p>Cześć,</p>
+            <input className={styles.input} type="text" placeholder="Podaj imię" />
+            <div className={styles.tiles}>
+                <div className={styles.bluePanel}>
+                    <p className={styles.p}>Dołącz jako:</p>
+                    <button className={styles.button} type="submit">Szef wywiadu</button>
+                    <button className={styles.button} type="submit">Agent</button>
+                </div>
+                <div className={styles.redPanel}>
+                    <p className={styles.p}>Dołącz jako:</p>
+                    <button className={styles.button} type="submit">Szef wywiadu</button>
+                    <button className={styles.button} type="submit">Agent</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Lobby;
