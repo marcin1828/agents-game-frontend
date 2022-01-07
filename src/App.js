d57f987a064e4e1631653892ce695fc8256751e9
Marcin Miernik <marcin.miernik2@gmail.com>
2022-01-07 02:00:20 +0100

    Enable StrictMode

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 const App = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<StartView />}/>
-            <Route path="lobby/:id" element={<Lobby />}/>
-            <Route path="game/:id" element={<GameService/>}/>
-        </Routes>
-    </BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<StartView />}/>
+                <Route path="lobby/:id" element={<Lobby />}/>
+                <Route path="game/:id" element={<GameService/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 )
 
 export default App;
